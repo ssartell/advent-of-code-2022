a6bbb81cd1acd4dff7ed9fab4b77beacf40af150
Sean Sartell <ssartell@rightpoint.com>
2022-12-04 17:16:24 -0600

    Fix overlaps

--- day04/part2.js
+++ day04/part2.js
@@ -3,7 +3,6 @@ import * as R from 'ramda';
 const readPair = R.pipe(R.split('-'), R.map(parseInt), R.zipObj(['low', 'high']));
 const parseInput = R.pipe(R.split('\n'), R.map(R.pipe(R.split(','), R.map(readPair))));
 
-const overlaps = (a, b) => a.low <= b.low && b.low <= a.high || a.low <= b.high && b.high <= a.high;
-const eitherOverlaps = (a, b) => overlaps(a, b) || overlaps(b, a);
+const eitherOverlaps = (a, b) => !(a.high < b.low || b.high < a.low);
 
 export default R.pipe(parseInput, R.filter(R.apply(eitherOverlaps)), R.length);
