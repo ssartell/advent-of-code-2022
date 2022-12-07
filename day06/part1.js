a483d60a60cdfbe4c426ff7d92937ddc66dc3b1b
Sean Sartell <ssartell@rightpoint.com>
2022-12-06 21:48:29 -0600

    update

--- day06/part1.js
+++ day06/part1.js
@@ -1,6 +1,4 @@
 import * as R from 'ramda';
 
 const markerSize = 4;
-const findFirstMarker = R.pipe(R.aperture(markerSize), R.find(R.pipe(R.uniq, R.length, R.equals(markerSize))), R.join(''));
-
-export default R.pipe(R.converge(R.indexOf, [findFirstMarker, R.identity]), R.add(markerSize));
+export default R.pipe(R.aperture(markerSize), R.findIndex(R.pipe(R.uniq, R.length, R.equals(markerSize))), R.add(markerSize));
