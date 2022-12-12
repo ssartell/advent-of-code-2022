4a5a67848a9961f6025689300d719d48be9ec754
Sean Sartell <ssartell@rightpoint.com>
2022-12-12 12:49:44 -0600

    day12

--- day12/part1.js
+++ day12/part1.js
@@ -1,20 +1,20 @@
 import * as R from 'ramda';
 import { bfs } from '../utils/graph-traversal.js';
-import { getValue, indexOf, getCardinalNeighbors, gridPositions, setValue } from '../utils/grid.js';
+import { getValue, indexOf, getCardinalNeighbors, setValue } from '../utils/grid.js';
 import { equals, toString } from '../utils/vec2.js';
 
-const toElevation = x => 'SabcdefghijklmnopqrstuvwxyzE'.indexOf(x);
-const parseInput = R.pipe(R.split('\n'), R.map(R.pipe(R.split(''), R.map(toElevation))));
+const parseInput = R.pipe(R.split('\n'), R.map(R.split('')));
 
 const findPath = grid => {
-  let startPos = { ...indexOf(grid, toElevation('S')), cost: 0, elevation: 'S' };
-  setValue(grid, startPos, toElevation('a'));
-  let endPos = indexOf(grid, toElevation('E'));
-  setValue(grid, endPos, toElevation('z'));
+  let startPos = { ...indexOf(grid, 'S'), cost: 0, height: 0 };
+  setValue(grid, startPos, 'a');
+  let endPos = indexOf(grid, 'E');
+  setValue(grid, endPos, 'z');
+
   const isEnd = n => equals(n, endPos);
   const getNeighbors = n => getCardinalNeighbors(grid, n)
-    .filter(x => getValue(grid, x) - getValue(grid, n) <= 1)
-    .map(x => ({ ...x, cost: n.cost + 1 }));
+    .map(x => ({ ...x, cost: n.cost + 1, height: getValue(grid, x).charCodeAt(0) - 97 }))
+    .filter(x => x.height - n.height <= 1);
   return bfs(startPos, isEnd, getNeighbors, toString);
 };
 
--- day12/part2.js
+++ day12/part2.js
@@ -3,16 +3,16 @@ import { bfs } from '../utils/graph-traversal.js';
 import { getValue, indexOf, getCardinalNeighbors, setValue } from '../utils/grid.js';
 import { toString } from '../utils/vec2.js';
 
-const toElevation = x => 'SabcdefghijklmnopqrstuvwxyzE'.indexOf(x);
-const parseInput = R.pipe(R.split('\n'), R.map(R.pipe(R.split(''), R.map(toElevation))));
+const parseInput = R.pipe(R.split('\n'), R.map(R.split('')));
 
 const findPath = grid => {
-  let startPos = { ...indexOf(grid, toElevation('E')), cost: 0, elevation: 'E' };
-  setValue(grid, startPos, toElevation('z'));
-  const isEnd = n => getValue(grid, n) === toElevation('a');
+  let startPos = { ...indexOf(grid, 'E'), cost: 0, height: 25 };
+  setValue(grid, startPos, 'z');
+
+  const isEnd = n => getValue(grid, n) === 'a';
   const getNeighbors = n => getCardinalNeighbors(grid, n)
-    .filter(x => getValue(grid, n) - getValue(grid, x) <= 1)
-    .map(x => ({ ...x, cost: n.cost + 1 }));
+  .map(x => ({ ...x, cost: n.cost + 1, height: getValue(grid, x).charCodeAt(0) - 97 }))
+  .filter(x => n.height - x.height <= 1);
   return bfs(startPos, isEnd, getNeighbors, toString);
 };
 
