718df86a19a0ad5cb4cc0e535bdc2eec47fb768f
Sean Sartell <ssartell@rightpoint.com>
2022-12-13 12:18:01 -0600

    day13

--- day13/part1.js
+++ day13/part1.js
@@ -1,22 +1,16 @@
 import * as R from 'ramda';
 
-const parseInput = R.pipe(R.split('\n\n'), R.pipe(R.map(R.pipe(R.split('\n'), R.map(eval)))));
+const parseInput = R.pipe(R.split('\n\n'), R.pipe(R.map(R.pipe(R.split('\n'), R.map(JSON.parse)))));
 
 export const packetComparator = (left, right) => {
   if (typeof left === 'number' && typeof right === 'number') {
     return Math.sign(left - right);
-  }
-  if (left instanceof Array && right instanceof Array) {
+  } else if (left instanceof Array && right instanceof Array) {
     let subComparison = R.map(R.apply(packetComparator), R.zip(left, right));
-    let inOrder = R.find(x => x !== 0, subComparison) || left.length - right.length;
-    if (inOrder !== 0) {
-      return inOrder;
-    }
-  }
-  if (left instanceof Array && typeof right === 'number') {
+    return R.find(x => x !== 0, subComparison) || Math.sign(left.length - right.length);
+  } else if (left instanceof Array && typeof right === 'number') {
     return packetComparator(left, [right]);
-  }
-  if (typeof left === 'number' && right instanceof Array) {
+  } else if (typeof left === 'number' && right instanceof Array) {
     return packetComparator([left], right);
   }
   return 0;
--- day13/part2.js
+++ day13/part2.js
@@ -4,7 +4,7 @@ import { packetComparator } from './part1.js';
 let divPacket1 = [[2]];
 let divPacket2 = [[6]];
 
-const parseInput = R.pipe(x => x.replaceAll('\n\n', '\n'), R.split('\n'), R.map(eval));
+const parseInput = R.pipe(x => x.replaceAll('\n\n', '\n'), R.split('\n'), R.map(JSON.parse));
 
 const getScore = packets => (R.indexOf(divPacket1, packets) + 1) * (R.indexOf(divPacket2, packets) + 1);
 
